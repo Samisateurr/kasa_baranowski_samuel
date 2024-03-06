ca7ef2b597e2cfdf275b2a187198a46899ec1c4b
Samisateurr <samuel.baranowski@orange.fr>
2024-03-07 00:16:33 +0100

    feat : test link dans Header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
+
 function App() {
   return (
     <Router>
